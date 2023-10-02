1b43cab249859f799467a751c15542baef7e585f
Collin <collin.j.gowdy@gmail.com>
2023-10-01 21:40:57 -0500

    Added navbar

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,6 @@
+<nav>
+    <a href="/">Home</a>
+    <a href="/auth">Login/Signup</a>
+</nav>
 <h1>My First Investment App</h1>
 <body>Let your allowance, birthday money, christmas money, lemonade money, <span style="font-style: italic">ANY</span> money make MORE money for you!</body>
