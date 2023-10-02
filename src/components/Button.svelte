5a2a5d632b04817af8d34aec032e811b93616c69
Collin <collin.j.gowdy@gmail.com>
2023-10-01 23:08:42 -0500

    Created Button Component

---
+++ src/components/Button.svelte
@@ -0,0 +1,8 @@
+<script>
+    import {createEventDispatcher} from 'svelte';
+	const buttonPressed = createEventDispatcher();
+	export let buttonText = '';
+</script>
+<button on:click={buttonPressed()}>
+    {buttonText}
+</button>
--- src/routes/auth/+page.svelte
+++ src/routes/auth/+page.svelte
@@ -1,4 +1,5 @@
 <script lang='ts'>
+    import Button from '../../components/Button.svelte';
     import {auth} from '../../firebase';
 	import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
 	import {goto} from "$app/navigation";
@@ -28,9 +29,42 @@
     }
 </script>
 
-<form>
-    <input type="email" bind:value={email} placeholder="Email" />
-    <input type="password" bind:value={password} placeholder="Password" />
-    <button class="bg-sky-500 hover:bg-sky-700 rounded px-2 py-1" type="button" on:click={login}>Login</button>
-    <button type="button" on:click={signUp}>Sign Up</button>
-</form>
+<div class="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+    <div class="sm:mx-auto sm:w-full sm:max-w-sm">
+        <img class="mx-auto h-10 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="Your Company">
+        <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Sign in to your account</h2>
+    </div>
+
+    <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+        <form class="space-y-6">
+            <div>
+                <label for="email" class="block text-sm font-medium leading-6 text-gray-900">Email address</label>
+                <div class="mt-2">
+                    <input id="email" bind:value={email} name="email" type="email" autocomplete="email" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                </div>
+            </div>
+
+            <div>
+                <div class="flex items-center justify-between">
+                    <label for="password" class="block text-sm font-medium leading-6 text-gray-900">Password</label>
+                    <div class="text-sm">
+                        <a  class="font-semibold text-indigo-600 hover:text-indigo-500">Forgot password?</a>
+                    </div>
+                </div>
+                <div class="mt-2">
+                    <input id="password" bind:value={password} name="password" type="password" autocomplete="current-password" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                </div>
+            </div>
+
+            <div>
+                <Button buttonText="Sign in" on:buttonPressed={login}/>
+<!--                <button type="submit" on:click={login} class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Sign in</button>-->
+            </div>
+        </form>
+
+        <p class="mt-10 text-center text-sm text-gray-500">
+            Not a member?
+            <a on:click={signUp} class="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">Start a 14 day free trial</a>
+        </p>
+    </div>
+</div>
